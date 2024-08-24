d48e0e0eca1b60ba9274acc70f93bc5352ead602
belligerentbeagle <ethan.wei@u.nus.edu>
2024-08-24 13:53:11 +0800

    More setups

---
+++ src/db_utils/db_cxn.py
@@ -0,0 +1,13 @@
+import sqlite3
+
+
+def setupDB():
+
+    con = sqlite3.connect("theGivingCook.db")
+
+    cur = con.cursor()
+
+    cur.execute("CREATE TABLE movie(title, year, score)")
+
+
+setupDB()
