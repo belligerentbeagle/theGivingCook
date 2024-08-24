465f369da00900a7f0931f137d21fa3c6a5adb57
ljlimjk <ljlimjk10@gmail.com>
2024-08-24 17:15:19 +0800

    update db_donor DatabaseConnector

--- src/db_utils/db_donors.py
+++ src/db_utils/db_donors.py
@@ -2,13 +2,12 @@ import sqlite3
 
 class DatabaseConnector:
     def __init__(self):
-        self.database_location = "../data/theGivingCook.db"
+        self.database_location = "src/data/theGivingCook.db"
 
     def connect(self):
         try:
-            conn = sqlite3.connect(self.database_loc)
+            conn = sqlite3.connect(self.database_location)
             return conn
         except Exception as e:
             print(f"Error connecting to database: {e}")
             return None
-        
