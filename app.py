5cf0c26f6d244a3fb663c742402b2a021e73f01a
ljlimjk <ljlimjk10@gmail.com>
2024-08-24 20:24:17 +0800

    extract query params for qr code scan handling

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+import streamlit as st
+
+from src.donor.qr_code_scan_handler import handle_qr_scan
+from src.donor.donor_utils import generate_qr_code
 from src.donor.donor_entry import init_donor_page
 from src.donor.home import run_home_page
 
@@ -6,5 +10,14 @@ from src.donor.home import run_home_page
 
 
 def run():
-    # initialize interface for donors
-    init_donor_page()
+    query_params = st.query_params
+    if query_params:
+        scan = query_params.get("scan", ["False"])[0].lower() == "true"
+        # either individual or ngo
+        collection_type = query_params.get("collection_type", [None])[0]
+        inventory_id = query_params.get("inventory_id", [None])[0]
+
+        if scan and inventory_id and collection_type:
+            handle_qr_scan(collection_type, inventory_id)
+    else:
+        init_donor_page()
