e0d669fdcffbd092632bf8c76486a73e8b5b160f
belligerentbeagle <ethan.wei@u.nus.edu>
2024-08-25 22:13:33 +0800

    Update Profile Mgmt

--- src/recipients/recipients_entry.py
+++ src/recipients/recipients_entry.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from src.recipients import search_food
 from src.recipients.report_form.report_form import file_report
+from src.ProfileManagement import profile_management
 
 def init_recipient_page():
     # Get user's name from session state and set default page if not set
@@ -23,6 +24,7 @@ def init_recipient_page():
     if st.session_state.page == 'Search Food':
         search_food.view_postings()
     elif st.session_state.page == 'Manage Profile':
+        profile_management()
         print("manage profile")
     elif st.session_state.page == 'Past Donations':
         print("past donations")
