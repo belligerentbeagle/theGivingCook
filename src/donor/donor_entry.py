20021762d5751c2f2b277c74e3fbf1f92bb9e016
oeggy03 <hathu2516@gmail.com>
2024-08-25 11:02:11 +0800

    SSet default page for donor_entry.py as Home.

--- src/donor/donor_entry.py
+++ src/donor/donor_entry.py
@@ -4,8 +4,7 @@ from src.donor.post_food.post_food_main import post_food
 
 
 def init_donor_page():
-    if 'page' not in st.session_state:
-        st.session_state.page = 'Home'
+    st.session_state.page = 'Home'
     name = st.session_state['name']
     st.sidebar.title(f"Welcome Back, {name}!")
 
